fe4696d01c48ae576574774c094380bef15021e2
trondstromlie <mybestlabs@gmail.com>
2020-12-09 15:32:16 +0100

    last cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component, useState , useRef} from 'react'
+import React, { Component } from 'react'
 
 import "./App.css";
 
 export class App extends Component {
-  constructor(props) {
+  intervalID = 0;
 
+  constructor(props) {
+    
     super(props);
     this.update_clock = this.update_clock.bind(this);
-
+   
+    
     
 
     // define the functions in the constructor to initialize the this keyword
@@ -18,9 +21,11 @@ export class App extends Component {
       timer_minutes:[0],
       timer_hours:[0],
       timer_days:[0],
-      timer_class: null,
+      timerOn:false
     }
   }
+ 
+
   componentDidMount() {
     this.timer()
     
@@ -30,9 +35,12 @@ export class App extends Component {
     
     
   }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
  
 
-  interval;
+  
 
   update_clock() {
     const { remaining_time } = this.state;
@@ -46,25 +54,25 @@ export class App extends Component {
   // Update state
   
 
-  if(this.state.timer_seconds[0] != seconds) {
+  if(this.state.timer_seconds[0] !== seconds) {
     this.setState({timer_seconds: [...this.state.timer_seconds, seconds ]});
     setTimeout(()=>{
       this.setState({timer_seconds: [ seconds ]});
     } ,500)
   }
-  if(this.state.timer_minutes[0] != minutes) {
+  if(this.state.timer_minutes[0] !== minutes) {
     this.setState({timer_minutes : [...this.state.timer_minutes, minutes ]});
     setTimeout(()=>{
       this.setState({timer_minutes: [ minutes ]});
     } ,500)
   }
-  if(this.state.timer_hours[0] != hours) {
+  if(this.state.timer_hours[0] !== hours) {
     this.setState({timer_hours: [...this.state.timer_hours, hours ]});
     setTimeout(()=>{
       this.setState({timer_hours: [ hours ]});
     } ,500)
   }
-  if(this.state.timer_days[0] != days) {
+  if(this.state.timer_days[0] !== days) {
     this.setState({timer_days: [...this.state.timer_days, days ]});
     setTimeout(()=>{
       this.setState({timer_days: [ days ]});
@@ -73,37 +81,35 @@ export class App extends Component {
   
   
   
-  console.log(this.state);
+  
 
 
   this.setState({remaining_time: remaining_time - 1000})
   }
 
-  timer() {
-    setInterval(this.update_clock,1000);
-  }
-
   
+
+  timer() {
+    this.intervalID = setInterval(this.update_clock,1000);
+    this.setState({timerOn: true})
+  }
 
 
 
   render() {
 
 
-    
-    
-    const {text_toggle,countDown} = this.state;
-
     return (
-    
+        
         <section className="timer-container ">
+        
           <section className="timer">
 
             <div>
-              {console.log("render")}
+             
               <i className=" mt-4 fab fa-react mr-3 fa-8x timer-icon"></i>
               <h2>Countdown timer</h2>
-            
+
               <p>Buildt in React</p>
               </div>
 
